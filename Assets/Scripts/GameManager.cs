adfc31dec8eb0c8e754a537502dc17faee826e9d
Ville <villehikaikkonen@gmail.com>
2024-09-11 09:06:08 +0300

    Added basic scenes and scripts
    Added Game Manager, and the bases for Main Menu and the Gladiator -minigame scenes.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/*
+Authors: Ville Kaikkonen
+Game Manager handles information across different scenes.
+*/
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager gameManager;
+
+    // Awake is called when the script is loaded
+    void Awake() {
+        if (gameManager == null)
+        {
+            Debug.Log("Preserving GameManager!");
+            gameManager = this;
+            DontDestroyOnLoad(this);
+        } else if (gameManager != this)
+        {
+            Debug.Log("Destroying GameManager!");
+            Destroy(gameObject);
+        }
+    }
+
+    // Loads a scene by its ID in scene hierarchy
+    public void LoadScene(int sceneID) { SceneManager.LoadScene(sceneID); }
+
+    // Loads a scene by its name
+    public void LoadScene(string sceneName) { SceneManager.LoadScene(sceneName); }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+Authors: Ville Kaikkonen
+Main Menu handles all the events in the "Main Menu" scene.
+*/
+
+public class MainMenu : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
