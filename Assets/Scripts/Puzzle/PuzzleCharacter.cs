18274b8fcd6865c3ccfb54e500cffef391872786
Ville <villehikaikkonen@gmail.com>
2024-09-13 10:33:00 +0300

    Puzzle movement finished
    Finished character movement and deleted deprecated test movement. Added collisions to the player character. Added initial prototype art for the character.

--- Assets/Scripts/Puzzle/PuzzleCharacter.cs
+++ Assets/Scripts/Puzzle/PuzzleCharacter.cs
@@ -18,25 +18,21 @@ public class PuzzleCharacter : MonoBehaviour
             if (Input.GetKey("up") && !Input.GetKey("down"))
             {
                 // Debug.Log("The character moves up.");
-                // characterPosition = new Vector2(x, y + (Time.deltaTime * moveSpeed));
                 y = y + (Time.deltaTime * moveSpeed);
             }
             if (Input.GetKey("down") && !Input.GetKey("up"))
             {
                 // Debug.Log("The character moves down.");
-                // characterPosition = new Vector2(x, y - (Time.deltaTime * moveSpeed));
                 y = y - (Time.deltaTime * moveSpeed);
             }
             if (Input.GetKey("right") && !Input.GetKey("left"))
             {
                 // Debug.Log("The character moves right.");
-                // characterPosition = new Vector2(x + (Time.deltaTime * moveSpeed), y);
                 x = x + (Time.deltaTime * moveSpeed);
             }
             if (Input.GetKey("left") && !Input.GetKey("right"))
             {
                 // Debug.Log("The character moves left.");
-                // characterPosition = new Vector2(x - (Time.deltaTime * moveSpeed), y);
                 x = x - (Time.deltaTime * moveSpeed);
             }
 
