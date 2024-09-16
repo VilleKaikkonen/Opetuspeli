64e3dc38820e2e83ccdd21b5a31df39bcf2bf806
Ville <villehikaikkonen@gmail.com>
2024-09-16 10:21:49 +0300

    Added interactable puzzle objects
    Added a script for objects, that react to other objects. Added a tag for "PuzzleObject", that marks objects that are reacted to.

---
+++ Assets/Scripts/Puzzle/PuzzleReactiveObject.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleReactiveObject : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("PuzzleObject"))
+        {
+            Debug.Log(collider.gameObject.name);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
