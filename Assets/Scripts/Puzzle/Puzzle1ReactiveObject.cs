ffdeee3a9672a0028054f4df802160380c05a0e9
Ville <villehikaikkonen@gmail.com>
2024-09-19 12:59:11 +0300

    Updated Puzzle minigame
    Added text elements to the Puzzle1 scene to show possible teaching methods in the game.

--- Assets/Scripts/Puzzle/Puzzle1ReactiveObject.cs
+++ Assets/Scripts/Puzzle/Puzzle1ReactiveObject.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 /*
 Author: Ville Kaikkonen
@@ -9,6 +10,7 @@ Detects when a puzzle object enters the object and then "opens" a door
 public class Puzzle1ReactiveObject : MonoBehaviour
 {
     [SerializeField] GameObject door;
+    [SerializeField] TMP_Text scriptText;
 
     // Called when a collider enters this object's trigger collider
     void OnTriggerEnter2D(Collider2D collider)
@@ -19,6 +21,12 @@ public class Puzzle1ReactiveObject : MonoBehaviour
             collider.GetComponent<Transform>().localPosition 
             = GetComponent<Transform>().localPosition;
             collider.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+            if (scriptText != null)
+            {
+                Color32 newColor = new Color32(255, 0, 13, 255);
+                scriptText.color = newColor;
+                collider.GetComponentInChildren<TMP_Text>().color = newColor;
+            }
             this.gameObject.SetActive(false);
         }
     }
